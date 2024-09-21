0a2e6c042cd7f4e4c55ef221f24608b9d0914c80
Alberto-16 <diego.moramoreno@ucr.ac.cr>
2024-09-20 22:40:50 -0600

    9-encabezados.h

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -4,14 +4,20 @@
 int main() {
 
     int v;
+    int m;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
 
+    m= funcion5(10);
+
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
+
+    // Imprime el valor que retornó funcion5()
+    printf("m = %d\n", m);
 
     return 0;
 
@@ -35,10 +41,17 @@ int funcion3(int x) {
     int y = x + 2;
     return y;
 }
-
-int funcion5(int x) {
+// Cambiamos el nombre de esta siguiente funcion para poder lograr ser uso de ese nombre en
+//en commit. funcion5 a funcionCambio5
+int funcionCambio5(int x) {
     printf("en funcion5");
 int funcion4(int x) {
     printf("En funcion4()");
     return x;
+}}
+
+int funcion5(int x) {
+    printf("En funcion5()...\n");
+    int y = x + 7;
+    return y;
 }
